db740b1c4e2c37217f9c8a22defb6f97cd5d12f1
MarcoGillone <marcogillone29@gmail.com>
2025-08-12 20:15:13 -0300

    first commit

---
+++ app/(tabs)/_layout.js
@@ -0,0 +1,11 @@
+import {Tabs} from 'expo-router';
+import {Ionicons} from '@expo/vector-icons';
+
+export default function TabsLayout (){
+    return(
+        <Tabs screenOptions={
+            
+        {}} />
+
+    );
+}
